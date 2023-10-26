2c430a9b1829eebc1fe55a656b12398414f65913
finni <nikalas.boyanov.nunev@cieep.com>
2023-10-26 12:17:00 +0200

    Initial Commit

---
+++ Ejercicio2ASwiftExamUITests/Ejercicio2ASwiftExamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ejercicio2ASwiftExamUITestsLaunchTests.swift
+//  Ejercicio2ASwiftExamUITests
+//
+//  Created by Finni on 26/10/23.
+//
+
+import XCTest
+
+final class Ejercicio2ASwiftExamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
