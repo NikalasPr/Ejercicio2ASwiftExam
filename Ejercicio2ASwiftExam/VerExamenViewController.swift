d382f743656639e90e596f69243fc8b0b7cdbcd0
finni <nikalas.boyanov.nunev@cieep.com>
2023-10-26 13:58:22 +0200

    2da ver

---
+++ Ejercicio2ASwiftExam/VerExamenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VerExamenViewController.swift
+//  Ejercicio2ASwiftExam
+//
+//  Created by Finni on 26/10/23.
+//
+
+import UIKit
+
+class VerExamenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
